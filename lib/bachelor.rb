2bd19f3ffcaebda2e304439f586f90d3f76e7c9f
Joe Greiner <jrgreiner115@gmail.com>
2018-03-26 17:31:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -26,7 +26,8 @@ def count_contestants_by_hometown(data, hometown)
   # code here
   counter = 0
   data.each do |season_data, player_data|
-    
+    player_data.each do |v|
+      v.
 end
 
 def get_occupation(data, hometown)
