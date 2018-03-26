f4b72fcf63934552e9bfe5758016854200f0d50b
Joe Greiner <jrgreiner115@gmail.com>
2018-03-26 17:48:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,7 +53,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_1, season_data|
     if season_1 == season
     season_data.each do |v|
-      new_array << v["age"].inject 
+      new_array << v["age"].inject(:+).to_f
       end
     end
     end
