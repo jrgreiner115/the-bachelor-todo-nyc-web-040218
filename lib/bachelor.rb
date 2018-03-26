75c00de1d23c2bc1539508305ff800e528d17671
Joe Greiner <jrgreiner115@gmail.com>
2018-03-26 17:15:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   # code here
   data.each do |season, season_data|
-    seaason_data.each do |
+    seaason_data.each do |k, v|
+      if k == "status"
+  end
+end
 end
 
 def get_contestant_name(data, occupation)
